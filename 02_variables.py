05536a1bad93cb9e2bd828fdd27ce4adcb398f16
baldockb2514 <111828862+baldockb2514@users.noreply.github.com>
2022-08-29 09:55:36 +1200

    Add files via upload

---
+++ 02_variables.py
@@ -0,0 +1,9 @@
+# code to learn about variables
+
+# they can also be numbers
+fav_num = 7
+
+#below is a constant (the name is in UPPERCASE)
+PI = 3.14
+
+num_2 =2
---
+++ 03_getting_input.py
@@ -0,0 +1,18 @@
+# Ask the user for their name
+username = input("What is your name? ")
+
+# Ask the user for their favout=rite interger
+fav_num = int(input("Favourite Number? "))
+
+# Double, halve and square the number
+double = fav_num * 2
+half = fav_num / 2
+squared = fav_num * fav_num
+
+print()
+# Greet the user
+print("Hi {}, your favourite number is {}" .format(username, fav_num))
+print()
+# Output the results of doubling, halving 
+# and squaring their favourite number
+print("double {}")
